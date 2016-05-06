6499b394ee103a59f10a758f9c494c2291d3b246
Nicolas Martyanoff <khaelin@gmail.com>
2016-05-06 18:09:53 +0200

    initial import

---
+++ client.go
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2016 Nicolas Martyanoff <khaelin@gmail.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package imapc
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net"
+)
+
+// ---------------------------------------------------------------------------
+//  Client state
+//  RFC 3501 3.
+// ---------------------------------------------------------------------------
+type ClientState string
+
+const (
+	ClientStateNotAuthenticated ClientState = "not-authenticated"
+	ClientStateAuthenticated    ClientState = "authenticated"
+	ClientStateSelected         ClientState = "selected"
+	ClientStateLogout           ClientState = "logout"
+)
+
+// ---------------------------------------------------------------------------
+//  Client
+// ---------------------------------------------------------------------------
+type Client struct {
+	Host       string
+	Port       int
+	TLS        bool
+	CACertPath string
+	CertPath   string
+	KeyPath    string
+
+	Conn net.Conn
+
+	State ClientState
+}
+
+func NewClient() *Client {
+	return &Client{
+		Host: "localhost",
+		Port: 143,
+	}
+}
+
+func (c *Client) Connect() error {
+	addr := fmt.Sprintf("%s:%d", c.Host, c.Port)
+
+	var err error
+	if c.TLS {
+		var caCerts *x509.CertPool
+		if c.CACertPath != "" {
+			caCertData, err := ioutil.ReadFile(c.CACertPath)
+			if err != nil {
+				return err
+			}
+
+			caCerts = x509.NewCertPool()
+			if caCerts.AppendCertsFromPEM(caCertData) == false {
+				return fmt.Errorf("cannot read ca certificate")
+			}
+		}
+
+		var cert tls.Certificate
+		if c.CertPath != "" && c.KeyPath != "" {
+			cert, err = tls.LoadX509KeyPair(c.CertPath, c.KeyPath)
+			if err != nil {
+				return err
+			}
+		}
+
+		cfg := tls.Config{
+			RootCAs:      caCerts,
+			Certificates: []tls.Certificate{cert},
+		}
+
+		c.Conn, err = tls.Dial("tcp", addr, &cfg)
+	} else {
+		c.Conn, err = net.Dial("tcp", addr)
+	}
+	if err != nil {
+		return err
+	}
+
+	c.State = ClientStateNotAuthenticated
+	return nil
+}
---
+++ utils/imapc/imapc.go
@@ -0,0 +1,83 @@
+//
+// Copyright (c) 2016 Nicolas Martyanoff <khaelin@gmail.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/galdor/imapc"
+	"github.com/pborman/getopt"
+)
+
+func main() {
+	// Command line
+	var printHelp bool
+
+	getopt.BoolVarLong(&printHelp, "help", 'h', "print help and exit")
+
+	getopt.CommandLine.SetParameters("<command> <args...>")
+	getopt.CommandLine.Parse(os.Args)
+
+	if printHelp {
+		getopt.CommandLine.PrintUsage(os.Stdout)
+		os.Exit(0)
+	}
+
+	args := getopt.CommandLine.Args()
+
+	if len(args) < 1 {
+		Die("missing argument")
+	}
+
+	cmd := args[0]
+	cmdArgs := args[1:]
+
+	// Command
+	var cmdFn func(*imapc.Client, []string)
+
+	switch cmd {
+	case "help":
+		getopt.CommandLine.PrintUsage(os.Stdout)
+		return
+	case "connect":
+		cmdFn = CmdConnect
+	default:
+		Die("unknown command")
+	}
+
+	// Client
+	client := imapc.NewClient()
+
+	if err := client.Connect(); err != nil {
+		Die("%v", err)
+	}
+
+	cmdFn(client, cmdArgs)
+}
+
+func CmdConnect(client *imapc.Client, args []string) {
+}
+
+func Error(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	fmt.Fprintf(os.Stderr, "%s\n", msg)
+}
+
+func Die(format string, args ...interface{}) {
+	Error(format, args...)
+	os.Exit(1)
+}
